70337e05bc4c0ae233895dc11bdb9fa93d9f161c
flyig <1158377828@qq.com>
2016-10-23 22:15:57 +0800

    update

--- tip-dialog/src/test/java/renthelper/tipdialog/BaseUnitTest.java
+++ tip-dialog/src/test/java/renthelper/tipdialog/BaseUnitTest.java
@@ -1,14 +1,6 @@
 package renthelper.tipdialog;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
 
 /**
  * Created with by shuangyao on 2016/10/21.
